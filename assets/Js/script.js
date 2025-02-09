4f223e42723b2458d9c9daf1f3b81fd66c861447
Segun David <segdavid03@gmail.com>
2025-02-09 17:52:09 +0100

    feat: implemented nav menu toggle state

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+// toggle on and off 
+function showMenuList(){
+    const nav = document.querySelector('#nav')
+    const show = document.querySelector('.show')
+    const remove = document.querySelector('.remove')
+    const navList = document.querySelector('.nav__list')
+
+    if(show){
+        show.addEventListener('click', ()=>{
+            navList.classList.add("show__menu");
+            show.classList.add('none')
+            remove.classList.remove('remove')
+        })
+    }
+
+    if(remove){
+        remove.addEventListener('click', ()=>{
+            navList.classList.remove("show__menu");
+            remove.classList.add('remove')
+            show.classList.remove('none')
+        })
+    }
+
+
+
+}
+
+showMenuList()
