d18b3a0b056dd05a0dc593b5e0bd9bf906e04522
Segun David <segdavid03@gmail.com>
2025-02-11 22:17:40 +0100

    feat: change background header on scroll

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -49,6 +49,23 @@ tab.forEach(t => {
             tab[1].classList.remove('active')
             tab[2].classList.remove('active')
             tab[0].classList.add('active')
-        }
+        }scroll-header
     })
 })
+
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.querySelector('header')
+
+    // when the scroll is greater than 80 viewport height, add the scoll-header class to the header tag
+    if(this.scrollY >= 80) {
+        header.classList.add('scroll-header')
+    } else{
+        header.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
+// 
